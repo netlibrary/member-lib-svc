b81b64073e7c9b2eb21a4402922117dac7a0b999
virsel <paul-hornig@gmx.de>
2023-12-25 11:08:50 +0100

    improved delete logic

--- apollo-neo4j/type-defs/general.ts
+++ apollo-neo4j/type-defs/general.ts
@@ -3,14 +3,29 @@ import { gql } from "apollo-server";
 export const general_typeDefs = gql`
   type Mutation {
     deleteCascade(id: ID!): Int
-        @cypher(
-            statement: """
-            MATCH (n:DeleteCascade { id: $id })
-            OPTIONAL MATCH (n)-[r*0..]->(sub)
-            DETACH DELETE n, sub
-            RETURN COUNT(sub) AS nodesDeleted
-            """
-            columnName: "nodesDeleted"
-        )
+      @cypher(
+        statement: """
+        MATCH (n:DeleteCascade { id: $id })
+        OPTIONAL MATCH (n)-[r*0..]->(sub)
+        DETACH DELETE n, sub
+        RETURN COUNT(sub) AS nodesDeleted
+        """
+        columnName: "nodesDeleted"
+      )
+    deleteListCascade(ids: [ID!]!): Int
+      @cypher(
+        statement: """
+        MATCH (n:DeleteCascade)
+        WHERE n.id IN $ids
+        OPTIONAL MATCH (n)-[r*0..]->(sub)
+        WITH n, COLLECT(sub) AS subNodes
+        DETACH DELETE n
+        WITH subNodes
+        UNWIND subNodes AS subNode
+        DETACH DELETE subNode
+        RETURN COUNT(DISTINCT subNode) AS nodesDeleted
+        """
+        columnName: "nodesDeleted"
+      )
   }
 `;
