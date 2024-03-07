a6c4fabec536a881ff7f08f9f6c701d84136d8a9
virsel <paul-hornig@gmx.de>
2024-03-07 17:19:43 +0100

    improved collection query

--- apollo-neo4j/type-defs/collection.ts
+++ apollo-neo4j/type-defs/collection.ts
@@ -17,6 +17,7 @@ export const collection_typeDefs = gql`
     id: ID!
     name: String!
     bookmarkCount: Int!
+    deepness: Int!
   }
 
   type CollectionDsList {
@@ -29,15 +30,17 @@ export const collection_typeDefs = gql`
         statement: """
         MATCH (m:Member {id: $memberId})-[:OWNS]->(c:Collection)
         OPTIONAL MATCH (m)-[:HAS]->(mm:MemberMeta)
-        WITH c, mm
+        OPTIONAL MATCH path=(c)-[:CONTAINS*0..]->(f:Folder)
+        WITH c, mm, length(path) AS depth
+        WITH c, mm, MAX(depth) AS maxDepth
         OPTIONAL MATCH (c)-[:CONTAINS*0..]->(:Folder)-[:CONTAINS*0..]->(b:Bookmark)
-        WITH c AS collection, COUNT(DISTINCT b) AS bookmarkCount, mm
-        WITH mm, COLLECT({id: collection.id, name: collection.name, bookmarkCount: bookmarkCount}) AS collections
+        WITH c AS collection, COUNT(DISTINCT b) AS bookmarkCount, mm, maxDepth + 1 AS deepness
+        WITH mm, COLLECT({id: collection.id, name: collection.name, bookmarkCount: bookmarkCount, deepness: deepness}) AS collections
         UNWIND mm.collectionPositions AS pos
         WITH pos, [collection IN collections WHERE collection.id = pos][0] AS sortedCollection
         WITH COLLECT(sortedCollection) AS sortedCollections
         RETURN {
-          collections: sortedCollections
+        collections: sortedCollections
         }  as r
         """
         columnName: "r"
